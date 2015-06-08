2fc4c7e50db0eec8549926e243a9b33f31cb29fc
nicorusti <nicolarustignoli@hotmail.it>
2015-06-08 10:40:38 +0200

    Clearer indent

--- src/main/java/it/polito/nexa/pc/businessEntities/Company.java
+++ src/main/java/it/polito/nexa/pc/businessEntities/Company.java
@@ -10,11 +10,12 @@ import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.StringUtils; 
 
-
-
 public class Company {
+	private String id; 
+	private boolean italian; 
 	private HashMap<String, Integer> names = new HashMap<>(); 
 	private HashMap<String, Integer> originalIds = new HashMap<>(); 
+	
 	public Company(String id, String name,  boolean isItalian) {
 		super();
 		this.id = id;
@@ -29,6 +30,7 @@ public class Company {
 	public String getId() {
 		return id;
 	}
+	
 	public boolean hasOriginalId(){
 		return !originalIds.isEmpty(); 
 	}
@@ -41,38 +43,46 @@ public class Company {
 		originalIds.put(originalId, 1); 
 	}
 	
-	private String id; 
-	private boolean italian; 
-	
 	public void addName(String name){
 		if ((names.containsKey(name) && (name.length()>1))){
 			names.put(name.toUpperCase(), names.get(name.toUpperCase())+1); 
-		} else {
+		} 	
+		else
+		{
 			if (name.length()>1)
-			names.put(name.toUpperCase(), 1);
+				names.put(name.toUpperCase(), 1);
 		}
 	}
+	
 	public void addOriginalId(String originalId){
 		if (originalIds.containsKey(originalId)){
 			originalIds.put(originalId, originalIds.get(originalId)+1); 
-		} else {
-			originalIds.put(originalId, 1);
-		}
+		} 
+		else 
+			originalIds.put(originalId, 1);	
 	}
+	
 	public HashMap<String, Integer> getNames(){
 		return names; 
 	}
+	
 	public Set<String> getOriginalIds(){
 		return originalIds.keySet(); 
 	}
+	
 	public String toString(){
 		return  
-		names.entrySet().stream().
-			    sorted(Entry.comparingByValue())
-			    .collect(Collectors.toMap(Entry::getKey, Entry::getValue,
-                        (e1, e2) -> e1, LinkedHashMap::new)).toString();   
-		
+		names.entrySet()
+			.stream()
+			.sorted(Entry.comparingByValue())
+			.collect(Collectors.toMap(Entry::getKey, Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new))
+			.toString();   		
 	}
+	
+	/*
+	 * returns the most likely label, based on 
+	 * label frequency and Levensthein distances
+	 * */
 	public String getBestLabel(){
 		double similarity; 
 		HashMap<String, Double> comparedNames = new HashMap<>(); 
@@ -81,18 +91,21 @@ public class Company {
 			for (Entry f : names.entrySet()){
 				if (!f.getKey().equals(e.getKey()))	{
 					similarity +=StringUtils.getLevenshteinDistance((String)e.getKey(), (String) f.getKey())*(Integer)e.getValue();
-				//System.out.println(e.getKey()+"---"+ f.getKey()); 
-				//System.out.println(StringUtils.getLevenshteinDistance((String)e.getKey(), (String) f.getKey())); 
 				}
 			}
 			comparedNames.put((String)e.getKey(), similarity); 	
 		}
-		//System.out.println(comparedNames); 
-		return comparedNames.entrySet().stream()
-	     .max(Comparator.comparing(e -> e.getValue())).get().getKey()
-	          ; 
-		
+		return comparedNames.entrySet()
+				.stream()
+				.max(Comparator.comparing(e -> e.getValue()))
+				.get()
+				.getKey(); 		
 	}
+	
+	/*
+	 * For debig purposes: returns all labels with relative score achieved 
+	 * by comparing algorythm
+	 * */
 	public HashMap<String, Double> getAllLabels(){
 		double similarity; 
 		HashMap<String, Double> comparedNames = new HashMap<>(); 
@@ -101,31 +114,12 @@ public class Company {
 			for (Entry f : names.entrySet()){
 				if (!f.getKey().equals(e.getKey()))	{
 					similarity +=StringUtils.getLevenshteinDistance((String)e.getKey(), (String) f.getKey())*(Integer)e.getValue();
-				//System.out.println(e.getKey()+"---"+ f.getKey()); 
-				//System.out.println(StringUtils.getLevenshteinDistance((String)e.getKey(), (String) f.getKey())); 
 				}
 			}
 			comparedNames.put((String)e.getKey(), similarity); 	
 		}
-		//System.out.println(comparedNames); 
 		return comparedNames; 
-		
 	}
 	
-	class ValueComparator implements Comparator<String> {
-
-	    Map<String, Double> base;
-	    public ValueComparator(Map<String, Double> base) {
-	        this.base = base;
-	    }
-
-	 
-
-		@Override
-		public int compare(String a, String b) {
-			Double result = base.get(a)-base.get(b); 
-			return  result.intValue() ; 
-		}
-	}
-
+	
 }
--- src/main/java/it/polito/nexa/pc/businessEntities/MainService.java
+++ src/main/java/it/polito/nexa/pc/businessEntities/MainService.java
@@ -17,84 +17,85 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
 public class MainService {
+	private static HashMap<String, Company> companies = new HashMap<>(); 
+	private HashMap <String, Object> result = new HashMap<>(); 
+	private static String filesDirectory="C:/pythonexercises/download/"; 
 	
-	static HashMap<String, Company> companies = new HashMap<>(); 
-	HashMap <String, Object> result = new HashMap<>(); 
-
 	public static void main(String[] args) {
-			
-	       DefaultJSONImporter dji = new DefaultJSONImporter();
-	       
-	    // Read all json files 
-	        long startTime = System.currentTimeMillis();
-	        long endTime = 0;
-	        System.out.println("Reading all files in directory..."); 
-
-	        File dir = new File("C:/pythonexercises/download/");
-	        Collection files = FileUtils.listFiles(dir, new RegexFileFilter("([^\\s]+(\\.(?i)(json))$)"), DirectoryFileFilter.DIRECTORY);
-	        System.out.println("Files found: "+files.size());
-	        Iterator itr = files.iterator();
-	        int processedFiles = 0;
-	        
-	        while (itr.hasNext()) {
-	            String value = itr.next().toString();
-	            Path path = Paths.get(value);
-	            String fileName = path.getFileName().toString();
-	            if(!fileName.equals("businessEntities.json") &&!fileName.equals("stats.json") && !fileName.equals("proposingStructure.json") && !fileName.equals("downloadInfo.json")&& !fileName.equals("downloadStats.json") && !fileName.contains("_index")){
-	                String pcJson = dji.getJSON(value, "FILE");
-	                //System.out.println(value); 
-	                companiesFromJson(pcJson); 
-	                processedFiles += 1;
-	                if (processedFiles %10000 == 0) {
-	                    System.out.println("Processed " + processedFiles +" files");
-	                }
-	                
-	            }
-	        }
-	        endTime = System.currentTimeMillis();
-	        
-	        //remove from the list, all the public administrations
-	        removePA("download/proposingStructures.json");
-	        
-	        LinkedList<HashMap<String, Object>> result =  companies.values().stream()
-	        .collect(Collector.of(
-	                () -> new LinkedList<HashMap<String, Object>>(),          // supplier
-	                (list, c) ->     {
+		
+		// Read all json files in the download directory 
+        long startTime = System.currentTimeMillis();
+        long endTime = 0;
+        System.out.println("Reading all files in directory..."); 
+        File dir = new File(filesDirectory);
+        Collection files = FileUtils.listFiles(dir, new RegexFileFilter("([^\\s]+(\\.(?i)(json))$)"), DirectoryFileFilter.DIRECTORY);
+        System.out.println("Files found: "+files.size());
+        Iterator itr = files.iterator();
+        int processedFiles = 0;
+        DefaultJSONImporter dji = new DefaultJSONImporter();
+       
+        //add all the companies found and relative labels to a list 
+        while (itr.hasNext()) {
+            String value = itr.next().toString();
+            Path path = Paths.get(value);
+            String fileName = path.getFileName().toString();
+            if(!fileName.equals("businessEntities.json") &&!fileName.equals("stats.json") && !fileName.equals("proposingStructure.json") 
+            		&& !fileName.equals("downloadInfo.json")&& !fileName.equals("downloadStats.json") && !fileName.contains("_index")){
+                String pcJson = dji.getJSON(value, "FILE");
+                companiesFromJson(pcJson); 
+                processedFiles += 1;
+                if (processedFiles %10000 == 0) {
+                    System.out.println("Processed " + processedFiles +" files");
+                }  
+            }
+        }
+        endTime = System.currentTimeMillis();
+        
+        //remove from the list, all the public administrations present in proposingStructures.json
+        removePA("download/proposingStructures.json");
+        
+        //
+        LinkedList<HashMap<String, Object>> result =  companies.values()
+        		.stream()
+        		.collect(Collector.of(() -> new LinkedList<HashMap<String, Object>>(), (list, c) ->
+	        		{
 	                	Company company = (Company) c; 
 	                	HashMap <String, Object> res = new HashMap<>(); 
 	                	res.put("id", company.getId()); 
-	                	if (company.hasOriginalId()) res.put("originalId", company.getOriginalIds()); 
+	                	if (company.hasOriginalId()) 
+	                		res.put("originalId", company.getOriginalIds()); 
 	                	res.put("name", company.getBestLabel()); 
 	                	res.put("isItalian", company.isItalian());
-	                	//uncomment this to write all labels (debug)
-	                	//res.put("name", company.getAllLabels()); 
 	                	list.add(res);
-	                /*System.out.println(p); */ } ,  // accumulator
-	                (j1, j2) ->{ j1.addAll(j2); 
-	                return j1; })); 
-	       
-	        System.out.println(String.format("Found %d distinct business entities.", result.size())); 
-	        ObjectMapper mapper =  new ObjectMapper().configure(SerializationFeature.INDENT_OUTPUT, true); 
-	      
-	        
-	       try {
-	    	  
-	        mapper.writeValue(new File("download/businessEntities.json"), result)
-	        ;
-	       } catch(Exception e ){
-	    	   System.out.println(e.getMessage()); 
-	       }
-	        //System.out.println(result); 		
-	        System.out.println("Time in minutes: "+ ((endTime-startTime)/1000)/60);
-
-	    
-	        
+	                 } ,  
+                (j1, j2) ->
+	                 { 
+	                	 j1.addAll(j2); 
+	                	 return j1; 
+	                 }
+	                 )
+        				); 
+       
+        System.out.println(String.format("Found %d distinct business entities.", result.size())); 
+        
+        //write list of companies to a json file 
+        ObjectMapper mapper =  new ObjectMapper().configure(SerializationFeature.INDENT_OUTPUT, true); 
+       try {  
+    	   mapper.writeValue(new File("download/businessEntities.json"), result); 
+        
+       } catch(Exception e ){
+    	   System.out.println(e.getMessage()); 
+       }		
+        System.out.println("Time in minutes: "+ ((endTime-startTime)/1000)/60);       
 	}
+	
+	
 	private static String getValue (String string, JsonNode record) {
         return record.get(string) != null ? record.get(string).asText() : "";
 	}
 	
-	/*this methos removes from the label list all the vatId which nelong to a public administration
+	/*
+	 * this methos removes from the label list all the vatId which nelong to a public administration
 	 * which are already present in the proposingStructure.json file 
 	 */
 		private static void removePA(String proposingStructuresFile){
@@ -122,52 +123,42 @@ public class MainService {
 	    ObjectMapper mapper = new ObjectMapper();
         try {
             JsonNode rootNode = mapper.readValue(inputJSON, JsonNode.class);
-            
             JsonNode data = rootNode.get("data").get("lotto");
             if (data !=null){
-            for (JsonNode record : data) {
-            	
-            	if(getValue("aggiudicatari",record) != null) {
-                    JsonNode winners = record.get("aggiudicatari");
-                    if (winners != null){ // XXX 
-                    	for (JsonNode winner:winners){
-                    		if(getValue("type", winner).equals("aggiudicatario")) {
-                    		addCompany(winner);               
-                    	}
-                    		if(getValue("type", winner).equals("aggiudicatarioRaggruppamento")) {
-                        		addGroup(winner);                         		
-                        	}     
-                }
-            	}
-
-                if(getValue("partecipanti",record) != null) {
-                    JsonNode participants = record.get("partecipanti");
-                    if (participants != null){ // XXX
-                    	for (JsonNode participant:participants){
-                    		if(getValue("type",  participant).equals("partecipante")) {
-                        		addCompany( participant); 
-                        		
-                        	}
-                        		if(getValue("type",  participant).equals("raggruppamento")) {
-                            		addGroup( participant);                            		
-                            	}
-                    		
-                    	}
-                        
-                }
-                }
-  		
-            	} 
-        }
+	            for (JsonNode record : data) {
+	            	if(getValue("aggiudicatari",record) != null) {
+	                    JsonNode winners = record.get("aggiudicatari");
+	                    if (winners != null){ 
+	                    	for (JsonNode winner:winners){
+	                    		if(getValue("type", winner).equals("aggiudicatario")) {
+	                    		addCompany(winner);               
+	                    		}
+	                    		if(getValue("type", winner).equals("aggiudicatarioRaggruppamento")) {
+	                        		addGroup(winner);                         		
+	                        	}     
+	                    	}
+	                    }
+	            	}
+	                if(getValue("partecipanti",record) != null) {
+	                    JsonNode participants = record.get("partecipanti");
+	                    if (participants != null){ 
+	                    	for (JsonNode participant:participants){
+	                    		if(getValue("type",  participant).equals("partecipante")) {
+	                        		addCompany( participant); 
+	                        	}
+	                        		if(getValue("type",  participant).equals("raggruppamento")) {
+	                            		addGroup( participant);                            		
+	                            	}                   		
+	                    	}                      
+	                    }
+	                }    	 
+	            }
             }
-            //companies.values().forEach(company-> System.out.println(company.getBestLabel()));
         }catch (Exception e) {
-	            e.printStackTrace();
-	            
-	        }
-        
-        
+        	e.printStackTrace();   
+	        }         
 	}
+	
 	private static void addCompany (JsonNode record){
 		String id; 
 		String name= getValue("ragioneSociale", record); 
@@ -176,45 +167,48 @@ public class MainService {
 		if(getValue("companyHash", record) != "") {
             id = getValue("companyHash", record);
             if(getValue("codiceFiscale", record) != "") {
-            originalId = getValue("codiceFiscale", record);
-            isItalian = true;
-                        }
+            	originalId = getValue("codiceFiscale", record);
+            	isItalian = true;
+                }
             if(getValue("identificativoFiscaleEstero", record) != "") {
                 originalId = getValue("identificativoFiscaleEstero", record);
                 isItalian = false;
-                            }
-        } else {
+                }
+        } 
+		else {
         	if (getValue("codiceFiscale", record)!=""){
         		id = getValue("codiceFiscale", record);
         		isItalian=true; 
-        	} else {
+        		} 
+        	else {
         		id = getValue("identificativoFiscaleEstero", record);
         		isItalian=false;
-        	}
-        	//if (id.length()<3) System.out.println(record);
+        		}
         }
 		
 		//add company to the list 
 		if (originalId==""){
-			if (companies.containsKey(id)) companies.get(id).addName(name);
+			if (companies.containsKey(id)) 
+				companies.get(id).addName(name);
 				else companies.put(id, new Company(id, name, isItalian)); 
-		} else{
+		} 
+		else{
 			if (companies.containsKey(id)) {
 				companies.get(id).addName(name);
 				companies.get(id).addOriginalId(originalId);
-			}
+				}
 			else companies.put(id, new Company(id, name, isItalian, originalId));
 		}
 	}
+	
 	private static void addGroup(JsonNode record){
 		JsonNode members ; 
-		if (record.has("aggiudicatarioRaggruppamento")) members =record.get("aggiudicatarioRaggruppamento"); 
-		 else  members =record.get("raggruppamento");
-
+		if (record.has("aggiudicatarioRaggruppamento")) 
+			members =record.get("aggiudicatarioRaggruppamento"); 
+		else  members =record.get("raggruppamento");
 		if (members!=null)
-		{
 			for (JsonNode member:members) addCompany(member); 
-		}
+		
 	}
 }
 
